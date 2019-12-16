fdb713d4f0eb776972e8b54cd1f2b84d6a33da64
kbonna <kongokou@gmail.com>
2019-12-17 00:24:12 +0100

    created first documentation and tests

--- chess_app/js/app.js
+++ chess_app/js/app.js
@@ -1,30 +1,14 @@
-Array.prototype.containsSubarray = function(subArray) {
-    for (let idx = 0; idx < this.length; idx++) {
-        let match = true;
-        for (let idy = 0; idy < subArray.length; idy++) {
-            if (this[idx][idy] !== subArray[idy]) {
-                match = false;
-                break;
-            }
-        }
-        if (match) {
-            return true;
-        }
-    }
-    return false;
-}
-
 /************
  * End Game *
  ************/
 
 function handleCheckMate(game, timer, ui) {
-    let endGameMsg; 
-        if (game.whoseTurn === -1) {
-            endGameMsg = 'Check mate! White won!';
-        } else {
-            endGameMsg = 'Check mate! Black won!';
-        }
+    let endGameMsg;
+    if (game.whoseTurn === -1) {
+        endGameMsg = 'Check mate! White won!';
+    } else {
+        endGameMsg = 'Check mate! Black won!';
+    }
     timer.togglePause();
     ui.toggleEnabled = false;
     window.alert(endGameMsg);
@@ -32,10 +16,11 @@ function handleCheckMate(game, timer, ui) {
 
 /*********************
  * Grab DOM elements *
- *********************/ 
+ *********************/
+
 const clockWhite = document.querySelector('.clock__time--white');
 const clockBlack = document.querySelector('.clock__time--black');
-const clockSetting = document.querySelector('#clock__setting')
+const clockSetting = document.querySelector('#clock__setting');
 
 const toggleButton = document.querySelector('#toggle');
 const restartButton = document.querySelector('#restart');
@@ -64,49 +49,59 @@ fieldList.forEach((field, idx) => {
 function draw() {
     for (let i = 0; i < 8; i++) {
         for (let j = 0; j < 8; j++) {
-
             switch (game.board[i][j]) {
                 case 0:
                     board[i][j].innerHTML = '';
                     break;
                 case 1:
-                    board[i][j].innerHTML = '<img src="img/pawn-white.svg" alt="white pawn" class="piece">';
+                    board[i][j].innerHTML =
+                        '<img src="img/pawn-white.svg" alt="white pawn" class="piece">';
                     break;
                 case -1:
-                    board[i][j].innerHTML = '<img src="img/pawn-black.svg" alt="black pawn" class="piece">';
+                    board[i][j].innerHTML =
+                        '<img src="img/pawn-black.svg" alt="black pawn" class="piece">';
                     break;
                 case 2:
-                    board[i][j].innerHTML = '<img src="img/bishop-white.svg" alt="white bishop" class="piece">';
+                    board[i][j].innerHTML =
+                        '<img src="img/bishop-white.svg" alt="white bishop" class="piece">';
                     break;
                 case -2:
-                    board[i][j].innerHTML = '<img src="img/bishop-black.svg" alt="black bishop" class="piece">';
+                    board[i][j].innerHTML =
+                        '<img src="img/bishop-black.svg" alt="black bishop" class="piece">';
                     break;
                 case 3:
-                    board[i][j].innerHTML = '<img src="img/knight-white.svg" alt="white knight" class="piece">';
+                    board[i][j].innerHTML =
+                        '<img src="img/knight-white.svg" alt="white knight" class="piece">';
                     break;
                 case -3:
-                    board[i][j].innerHTML = '<img src="img/knight-black.svg" alt="black knight" class="piece">';
+                    board[i][j].innerHTML =
+                        '<img src="img/knight-black.svg" alt="black knight" class="piece">';
                     break;
                 case 4:
-                    board[i][j].innerHTML = '<img src="img/rock-white.svg" alt="white rock" class="piece">';
+                    board[i][j].innerHTML =
+                        '<img src="img/rock-white.svg" alt="white rock" class="piece">';
                     break;
                 case -4:
-                    board[i][j].innerHTML = '<img src="img/rock-black.svg" alt="black rock" class="piece">';
+                    board[i][j].innerHTML =
+                        '<img src="img/rock-black.svg" alt="black rock" class="piece">';
                     break;
                 case 5:
-                    board[i][j].innerHTML = '<img src="img/queen-white.svg" alt="white queen" class="piece">';
+                    board[i][j].innerHTML =
+                        '<img src="img/queen-white.svg" alt="white queen" class="piece">';
                     break;
                 case -5:
-                    board[i][j].innerHTML = '<img src="img/queen-black.svg" alt="black queen" class="piece">';
+                    board[i][j].innerHTML =
+                        '<img src="img/queen-black.svg" alt="black queen" class="piece">';
                     break;
                 case 6:
-                    board[i][j].innerHTML = '<img src="img/king-white.svg" alt="white king" class="piece">';
+                    board[i][j].innerHTML =
+                        '<img src="img/king-white.svg" alt="white king" class="piece">';
                     break;
                 case -6:
-                    board[i][j].innerHTML = '<img src="img/king-black.svg" alt="black king" class="piece">';
+                    board[i][j].innerHTML =
+                        '<img src="img/king-black.svg" alt="black king" class="piece">';
                     break;
             }
-
         }
     }
 }
@@ -120,7 +115,7 @@ function highlightMoves(moves) {
 function cancelHighlight() {
     board.forEach(row => {
         row.forEach(field => {
-            field.classList.remove('highlight')
+            field.classList.remove('highlight');
         });
     });
 }
@@ -134,7 +129,6 @@ function hidePromotionModal() {
 }
 
 function performPromotion() {
-
     if (ui.currentPiece < 0) {
         promotionPieces[0].src = 'img/bishop-black.svg';
         promotionPieces[1].src = 'img/knight-black.svg';
@@ -161,13 +155,13 @@ const ui = {
     moveFrom: [],
     moveTo: [],
     currentPiece: null
-}
+};
 
 /**********************
  * Initialize objects *
  **********************/
 
-let game = new Game;
+let game = new Game();
 let timer = new Timer(game, ui, clockWhite, clockBlack);
 
 draw();
@@ -177,7 +171,6 @@ draw();
  *******************/
 
 toggleButton.addEventListener('click', e => {
-
     if (!ui.gameStarted) {
         timer.maxTime = clockSetting[clockSetting.selectedIndex].value * 60;
         ui.gameStarted = true;
@@ -202,10 +195,10 @@ restartButton.addEventListener('click', e => {
     toggleButton.innerHTML = 'Start';
     ui.gameStarted = false;
     clockSetting.disabled = false;
-    
+
     game.resetGame();
     timer.resetTimer();
-    
+
     ui.toggleEnabled = true;
     cancelHighlight();
     draw();
@@ -215,49 +208,53 @@ swapButton.addEventListener('click', e => {
     board.reverse();
     reversed = !reversed;
     draw();
-})
+});
 
 promotionPieces.forEach(p => {
     p.addEventListener('click', e => {
-
-        // Change promotion choice for promoting side 
+        // Change promotion choice for promoting side
         switch (e.target.id) {
             case 'promo-bishop':
-                game.promotion[Math.sign(ui.currentPiece)] = Math.sign(ui.currentPiece) * 2;
+                game.promotion[Math.sign(ui.currentPiece)] =
+                    Math.sign(ui.currentPiece) * 2;
                 break;
             case 'promo-knight':
-                game.promotion[Math.sign(ui.currentPiece)] = Math.sign(ui.currentPiece) * 3;
+                game.promotion[Math.sign(ui.currentPiece)] =
+                    Math.sign(ui.currentPiece) * 3;
                 break;
             case 'promo-rock':
-                game.promotion[Math.sign(ui.currentPiece)] = Math.sign(ui.currentPiece) * 4;
+                game.promotion[Math.sign(ui.currentPiece)] =
+                    Math.sign(ui.currentPiece) * 4;
                 break;
             case 'promo-queen':
-                game.promotion[Math.sign(ui.currentPiece)] = Math.sign(ui.currentPiece) * 5;
+                game.promotion[Math.sign(ui.currentPiece)] =
+                    Math.sign(ui.currentPiece) * 5;
                 break;
         }
 
         game.blockGame = false;
         hidePromotionModal();
-        
+
         // Proceed with the move
         game.move(ui.moveFrom, ui.moveTo);
-        if (game.checkMate) { handleCheckMate(game, timer, ui) };
+        if (game.checkMate) {
+            handleCheckMate(game, timer, ui);
+        }
         draw();
-
     });
 });
 
 board.forEach((row, ii) => {
     row.forEach((field, j) => {
         field.addEventListener('click', e => {
-
             // Interpreting clicks for upside down board (change first coordinate)
             let i = ii;
-            if (reversed) {i = 7 - ii;}
+            if (reversed) {
+                i = 7 - ii;
+            }
 
             // Show possible moves for piece
             if (!ui.highlight) {
-
                 ui.moveFrom = [i, j];
                 ui.currentPiece = game.board[i][j];
                 ui.movesAllowed = game.getMoves(i, j);
@@ -268,21 +265,25 @@ board.forEach((row, ii) => {
                     ui.highlight = true;
                 }
 
-            // Perform move if possible
+                // Perform move if possible
             } else {
-
                 ui.moveTo = [i, j];
 
                 if (ui.movesAllowed.containsSubarray(ui.moveTo)) {
-                    // Open promotion dialog if needed 
-                    if ((Math.abs(ui.currentPiece) === 1) & (ui.moveTo[0] === 0 | ui.moveTo[0] === 7)) {
+                    // Open promotion dialog if needed
+                    if (
+                        (Math.abs(ui.currentPiece) === 1) &
+                        ((ui.moveTo[0] === 0) | (ui.moveTo[0] === 7))
+                    ) {
                         performPromotion();
                     }
                     timer.cacheTime();
                     game.move(ui.moveFrom, ui.moveTo);
-                    if (game.checkMate) { handleCheckMate(game, timer, ui) };
+                    if (game.checkMate) {
+                        handleCheckMate(game, timer, ui);
+                    }
                     draw();
-                } 
+                }
                 cancelHighlight();
                 ui.highlight = false;
             }
--- chess_app/js/game.test.js
+++ chess_app/js/game.test.js
@@ -1,31 +1,80 @@
+const Game = require('./game.js');
 
-const game = new Game();
+beforeAll(() => {
+    return (game = new Game());
+});
+
+afterEach(() => {
+    game.resetGame();
+});
+
+// containsSubarray function
+
+test('containsSubarray producing correct output', () => {
+    const myArr = [
+        [0, 0],
+        [1, 1]
+    ];
+    const result_true = myArr.containsSubarray([0, 0]);
+    const result_false = myArr.containsSubarray([2, 2]);
+    expect(result_true).toBe(true);
+    expect(result_false).toBe(false);
+    expect(() => {
+        const result_wrong = myArr.containsSubarray(null);
+    }).toThrow(TypeError);
+});
+
+// populateBoard
+
+test('resetting chessboard', () => {
+    game.board[0] = [0, 0, 0, 0, 0, 0, 0, 0];
+    game.populateBoard();
+    expect(game.board[0]).toEqual([4, 3, 2, 5, 6, 2, 3, 4]);
+    expect(game.board[7]).toEqual([-4, -3, -2, -5, -6, -2, -3, -4]);
+});
+
+// clearBoard
+
+test('removing all pieces from chessboard', () => {
+    game.clearBoard();
+    for (let i = 0; i < 8; i++) {
+        for (let j = 0; j < 8; j++) {
+            expect(game.board[i][j]).toBe(0);
+        }
+    }
+});
+
+// toggleBlockGame
+
+test('toggling game blocked state', () => {
+    expect(game.isBlocked).toBe(true);
+    game.toggleBlockGame();
+    expect(game.isBlocked).toBe(false);
+});
 
 // getPawnMoves
-test('pawn move from first line (white)', () => {
-    game.clearBoard();
-    game.board[1][0] = 1;
-    expect(game.getPawnMoves(1, 0).sort()).toEqual([[3, 0], [2, 0]].sort());
-});
+// test('pawn move from first line (white)', () => {
+//     game.clearBoard();
+//     game.board[1][0] = 1;
+//     expect(game.getPawnMoves(1, 0).sort()).toEqual([[3, 0], [2, 0]].sort());
+// });
 
-test('pawn move from first line (black)', () => {
-    game.clearBoard();
-    game.board[6][1] = -1;
-    expect(game.getPawnMoves(6, 1).sort()).toEqual([[5, 1], [4, 1]].sort());
-});
+// test('pawn move from first line (black)', () => {
+//     game.clearBoard();
+//     game.board[6][1] = -1;
+//     expect(game.getPawnMoves(6, 1).sort()).toEqual([[5, 1], [4, 1]].sort());
+// });
 
-test('pawn blocked by ally)', () => {
-    game.clearBoard();
-    game.board[2][6] = 1;
-    game.board[3][6] = 1;
-    expect(game.getPawnMoves(2, 6)).toEqual([]);
-});
+// test('pawn blocked by ally)', () => {
+//     game.clearBoard();
+//     game.board[2][6] = 1;
+//     game.board[3][6] = 1;
+//     expect(game.getPawnMoves(2, 6)).toEqual([]);
+// });
 
-test('pawn with attack possibility near board edge', () => {
-    game.clearBoard();
-    game.board[5][7] = -1;
-    game.board[4][6] = 1;
-    expect(game.getPawnMoves(5, 7).sort()).toEqual([[4, 6], [4, 7]].sort());
-});
-
-
+// test('pawn with attack possibility near board edge', () => {
+//     game.clearBoard();
+//     game.board[5][7] = -1;
+//     game.board[4][6] = 1;
+//     expect(game.getPawnMoves(5, 7).sort()).toEqual([[4, 6], [4, 7]].sort());
+// });
